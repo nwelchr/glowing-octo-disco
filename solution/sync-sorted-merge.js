875a84b01d920cc1d90ab22da472b754c7048fa4
Nick Welch <nwelchr@gmail.com>
2024-07-16 09:42:20 -0700

    Naive solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,27 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const logEntries = [];
+
+  for (const logSource of logSources) {
+    let logEntry = logSource.pop();
+    while (!logSource.drained) {
+      logEntries.push(logEntry);
+      logEntry = logSource.pop();
+    }
+  }
+
+  logEntries.sort((a, b) => a.date - b.date);
+
+  for (const logEntry of logEntries) {
+    try {
+      printer.print(logEntry);
+    } catch (e) {
+      console.log(logEntry);
+    }
+  }
+
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
